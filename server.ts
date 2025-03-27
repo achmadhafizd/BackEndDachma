34404e0af57823f7b6f57349fc4d71dfaa75bec8
achmadhafizd <ahmadhafizd2104@gmail.com>
2025-03-27 17:33:25 +0700

    Remove 'export default app' for Vercel compatibility

--- server.ts
+++ server.ts
@@ -57,5 +57,3 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.message);
   res.status(500).json({ message: "Internal Server Error" });
 });
-
-export default app
