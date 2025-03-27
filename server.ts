621496808917ea397846c0d18a19a1eb6fae7692
achmadhafizd <ahmadhafizd2104@gmail.com>
2025-03-27 17:25:14 +0700

    Add vercel.json

--- server.ts
+++ server.ts
@@ -57,3 +57,5 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.message);
   res.status(500).json({ message: "Internal Server Error" });
 });
+
+export default app
