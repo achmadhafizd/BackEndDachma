b57b515c746813a2f5eab35328595b554f8e54ff
achmadhafizd <ahmadhafizd2104@gmail.com>
2025-04-10 07:39:53 +0700

    build: add netlify config

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,6 +1,9 @@
-exports.handler = async function () {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from the backend!" }),
+// netlify/functions/hello.js
+exports.handler = async () => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello from Netlify Functions!" }),
+    };
   };
-};
+  
+  module.exports.handler = exports.handler;  
